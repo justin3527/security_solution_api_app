3fe68f8a897675171c98cd3c7583e9ec5915c23e
justin3527 <skykings5@naver.com>
2019-02-20 14:27:42 +0900

    urls py edit

--- solution/forms.py
+++
@@ -1,5 +0,0 @@
-class PostForm(forms.ModelForm):
-
-    class Meta:
-        model = GuessNumbers
-        field = ('name', 'text',)
--- solution/urls.py
+++ solution/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from solution import views
 from . import views
 
 urlpatterns = [
