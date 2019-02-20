8759c4ddbb8e5a1e95a237e43efb55a288b25455
justin3527 <skykings5@naver.com>
2019-02-20 15:06:22 +0900

    d

--- solution/views.py
+++ solution/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-from . models import GuessNumbers
+from .models import GuessNumbers
 from .form import PostForm
 
 def test(request):
